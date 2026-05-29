ee70ad3af5a7a242efe3712462743cc83e56a073
EmnaMahat <mahat.emna@gmail.com>
2026-05-29 11:02:23 -0500

    Add home route

--- server.js
+++ server.js
@@ -10,18 +10,22 @@ const app = express();
 // Middleware
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('API Ecommerce is running 🚀');
+});
+
 // Routes
 app.use("/api/products", productRoutes);
 
 // Start server immediately (Render requirement)
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.log(err));
 
 module.exports = app;
